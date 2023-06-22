a5b86d6297ba614bd45a209a4ae7eec2d4785d83
Kartik <kbhasin2003@gmail.com>
2023-06-23 02:17:20 +0530

    committing

--- src/App.js
+++ src/App.js
@@ -3,38 +3,55 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import About from './Components/About';
-import { useState } from 'react';
+import Alert from './Components/Alert';
+import react, { useState } from 'react';
+
 
 function App() {
-  const [darkMode,setdarkMode]=useState(false);
-  const [mode,setMode]=useState('dark');
+  // const [darkMode,setdarkMode]=useState(false);
+  const [mode,setMode]=useState('light');
   const [switchED,EDswitch]=useState('DarkMode');
-
+const [alert,setAlert]=useState(null);
+const showAlert=(message,type)=>{
+setAlert(
+  {
+    msg:message,
+type:type
+  }
+)
+setTimeout(()=>{
+setAlert(null);
+},1000)
+}
   const toggleMode=()=>{
 if(mode==='dark'){
   setMode('light');
   document.body.style.backgroundColor='white';
   EDswitch('DarkMode');
+  showAlert("Light Mode Enabled","success");
 }
 else{
   setMode('dark');
   document.body.style.backgroundColor='#2f2a4b';
 EDswitch('LightMode');
+showAlert("Dark Mode Enabled","success");
+
 }
   }
 
   return (
-    <>
-    {/* // <Navbar></Navbar> */}
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} switchED={switchED}/>
+   
+  <>
+  <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} switchED={switchED}/>
+    <Alert alert={alert}/>
 <div className="container" >
-<TextForm heading="Enter Text Here" mode={mode} toggleMode={toggleMode}/>;
-{/* <About/> */}
+<TextForm heading="Enter Text Here" mode={mode} toggleMode={toggleMode}/>
+<About/>
 </div>
-    
-    </>
+
+  </>
   
   );
-}
 
+  }
 export default App;
